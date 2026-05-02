fafafffaa2d4293da05ddd2dea8be1c6a2dc4ff6
Yar384 <aroslavbelka83@gmail.com>
2026-05-02 17:22:33 +0300

    Create Models.cs

---
+++ Models.cs
@@ -0,0 +1,17 @@
+namespace TeacherApp
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
