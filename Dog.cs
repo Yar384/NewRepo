ff5cf18a8adf77884b81dc0bb9753f7f73eed9a3
Yar384 <aroslavbelka83@gmail.com>
2026-05-04 17:08:23 +0300

    Create Dog.cs

---
+++ Dog.cs
@@ -0,0 +1,11 @@
+namespace DogApp
+{
+    public class Dog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Breed { get; set; }
+        public bool IsAdopted { get; set; }
+    }
+}
